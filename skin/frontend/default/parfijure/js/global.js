a2abab25b93a7536c4c33be4a9c9f4d697d6ee9a
yejian <yejian122@qq.com>
2014-05-30 17:27:32 +0800

    [mod] animation of top navigation

--- skin/frontend/default/parfijure/js/global.js
+++ skin/frontend/default/parfijure/js/global.js
@@ -1,15 +1,15 @@
 (function($) {
   $(function() {
     // navigation
-    $('.top_links > .links > li').hover(
+    /*$('.top_links > .links > li').hover(
       function() {
         $(this).find('.level2').show();
       },
+
       function() {
         $(this).find('.level2').hide();
       }
-    );
-
+    );*/
     // banner click
     $('.fullwidthbanner ul li img').on('click', function() {
       window.location = $(this).data('href');
